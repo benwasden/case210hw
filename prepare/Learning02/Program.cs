5735d020d8b156a5398bf0ccfacf992625dc8587
benwasden <was24001@byui.edu>
2024-09-24 20:41:13 -0700

    Did Week 2 Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "School District";
+        job1._jobTitle = "Computer Technician";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Developer";
+        job2._startYear = 2005;
+        job2._endYear = 2015;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ben Wasden";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
