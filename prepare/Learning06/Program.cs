a62ad31b6d316ff434a349b01e9e4deb032f920e
benwasden <was24001@byui.edu>
2024-10-22 18:04:24 -0700

    Polymorphism Learning

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square square = new Square("Red", 5);
+        shapes.Add(square);
+        
+        Rectangle rectangle = new Rectangle("Blue", 3, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 4);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
